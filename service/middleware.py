a445d94e57425a94442d93a07c8cb5dd91274497
Tannous Geagea <tannous.geagea@wasteant.com>
2025-10-07 18:28:04 +0000

    fixed typos

--- service/middleware.py
+++ service/middleware.py
@@ -5,6 +5,7 @@ import logging
 import time
 import uuid
 
+from typing import Any
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 from starlette.responses import Response
